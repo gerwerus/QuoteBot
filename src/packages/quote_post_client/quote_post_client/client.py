73e52f4f6b3191d7569777d81385b086fbbbc68c
= <prosyannikov1978@mail.ru>
2024-05-03 21:53:54 +0700

    codestyle

--- src/packages/quote_post_client/quote_post_client/client.py
+++ src/packages/quote_post_client/quote_post_client/client.py
@@ -27,15 +27,3 @@ class QuoteGeneratorClient:
             image_text_bot.image_place_text(
                 text=post[0],
             )
-
-
-async def main():
-    q = UnsplashClient()
-    loop = asyncio.get_event_loop()
-    task = loop.create_task(q.get_by_keyword("jesus", 3, 600))
-    text = await asyncio.gather(task)
-    print(text)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
