dd7b7ed9af7960520eea5e46e07c09f2848fc8f9
Ekaterina703 <Ekaterina703>
2024-05-11 13:28:20 +0700

    ADD patch qoute method

--- src/webapp/quotes/router.py
+++ src/webapp/quotes/router.py
@@ -1,10 +1,10 @@
 from config.database import get_async_session
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .models import Post
-from .schemas import PostSchemaCreate, PostSchemaRead, PostQueryParams
+from .schemas import PostSchemaCreate, PostSchemaRead, PostSchemaUpdate, PostQueryParams
 
 router = APIRouter(
     prefix="/quotes",
@@ -29,3 +29,16 @@ async def create_quote(quote: PostSchemaCreate, session: AsyncSession = Depends(
     await session.commit()
     await session.refresh(new_quote)
     return new_quote
+
+
+@router.patch("/{quote_id}", status_code=status.HTTP_200_OK)
+async def update_quote(quote_id: int, quote: PostSchemaUpdate, session: AsyncSession = Depends(get_async_session)) -> PostSchemaRead:
+    query = select(Post).where(Post.id == quote_id)
+    result = await session.execute(query)
+    quote_to_update = result.scalars().first()
+    if not quote_to_update:
+        return Response(content={"message": "Quote not found"}, status_code=status.HTTP_404_NOT_FOUND)
+    quote_to_update.update(quote.model_dump())
+    await session.commit()
+    await session.refresh(quote_to_update)
+    return quote_to_update
