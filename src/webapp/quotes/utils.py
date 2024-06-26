07a4a372bee801be53f7026cf9e23a2358b73cbd
Ekaterina703 <Ekaterina703>
2024-06-26 21:30:02 +0700

    ADD quiz model, schema, endpoint

---
+++ src/webapp/quotes/utils.py
@@ -0,0 +1,15 @@
+from random import shuffle
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .models import Post
+from .schemas import QuizSchemaRead
+
+
+async def set_quiz_answers(quiz: QuizSchemaRead, session: AsyncSession) -> None:
+    query = select(Post.author).where(Post.author != quiz.author).limit(100)
+    result = await session.execute(query)
+    authors = result.scalars().all()
+    shuffle(authors)
+    quiz.answers = authors[:3]
